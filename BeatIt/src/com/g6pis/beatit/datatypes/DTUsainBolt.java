309421bfc48af832d2f4cb84c19770a8872a8c3c
martin-steglich <77.tincho@gmail.com>
2014-09-15 15:46:45 -0300

    Me faltó subir este datatype

---
+++ BeatIt/src/com/g6pis/beatit/datatypes/DTUsainBolt.java
@@ -0,0 +1,46 @@
+package com.g6pis.beatit.datatypes;
+
+public class DTUsainBolt extends DTChallenge {
+	
+	private double maxSpeed;
+	private double avgSpeed;
+	private double score;
+	
+	
+	public DTUsainBolt() {
+		super();
+
+	}
+
+	public double getMaxSpeed() {
+		return maxSpeed;
+	}
+
+
+	public void setMaxSpeed(double maxSpeed) {
+		this.maxSpeed = maxSpeed;
+	}
+
+
+	public double getAvgSpeed() {
+		return avgSpeed;
+	}
+
+
+	public void setAvgSpeed(double avgSpeed) {
+		this.avgSpeed = avgSpeed;
+	}
+
+
+	public double getScore() {
+		return score;
+	}
+
+
+	public void setScore(double score) {
+		this.score = score;
+	}
+
+	
+
+}
