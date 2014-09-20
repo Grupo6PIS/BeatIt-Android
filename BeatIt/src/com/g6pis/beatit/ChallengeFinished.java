3533c7bfcd92a68e3fe04f25831241f5ebc3d435
Alejandro B <alejandrobi89@gmail.com>
2014-09-20 20:48:28 -0300

    Se elimina la opción de Ajustes del Desafío Finalizado

--- BeatIt/src/com/g6pis/beatit/ChallengeFinished.java
+++ BeatIt/src/com/g6pis/beatit/ChallengeFinished.java
@@ -62,7 +62,7 @@ public class ChallengeFinished extends Activity implements OnClickListener {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.challenge_finished, menu);
+		// getMenuInflater().inflate(R.menu.challenge_finished, menu);
 		return true;
 	}
 
