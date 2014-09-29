b82a3aca716cd720acf8adb3a703bcbb35bf1c31
Felipe García <felipegarcia92@gmail.com>
2014-09-29 18:45:17 -0300

    Subo el proyecto FacebookSDK con referencia nueva, debería andar en el repositorio

---
+++ facebook-android-sdk-3.18.1/facebook/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\Users\Felipe\Documents\GitHub\facebook-android-sdk-3.18.1\facebook\bin\AndroidManifest.xml : \
+C:\Users\Felipe\Documents\GitHub\facebook-android-sdk-3.18.1\facebook\AndroidManifest.xml \
